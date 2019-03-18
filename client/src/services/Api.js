52c4b5f3f390ea45531d5c3d863b8fbeb29b53fa
Kevin <tello3000@hotmail.com>
2019-03-18 09:41:32 -0400

    Update: Implementing CRUD with Mongoose WIP

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-
 export default() => {
   return axios.create({
-    baseURL: 'pensivewright.com'
+    //baseURL: 'pensivewright.com'
+    baseURL: 'http://localhost:8081'
   })
 }
--- client/src/services/PostsService.js
+++ client/src/services/PostsService.js
@@ -1,6 +1,6 @@
 import Api from '@/services/Api'
 
-
+console.log(Api());
 export default {
   fetchPosts () {
     return Api().get('posts')
