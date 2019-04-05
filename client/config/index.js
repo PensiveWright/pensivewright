ee4941ad39a218c67f820bdc562690b11269556c
Kevin <tello3000@hotmail.com>
2019-04-05 17:00:21 -0400

    Update:

--- client/config/index.js
+++ client/config/index.js
@@ -3,7 +3,7 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 
 const path = require('path')
-const host = 'localhost'
+const host = '127.0.0.1'
 const port = 8080
 
 module.exports = {
