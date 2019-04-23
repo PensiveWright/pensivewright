12394c06fd97e867f8e11a744e0ff204d5b9dcbb
Kevin <tello3000@hotmail.com>
2019-04-23 14:05:39 -0400

    update

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default() => {
   return axios.create({
-    baseURL: 'http://www.pensivewright.com/serv'
-    //baseURL: 'http://localhost:8081'
+    //baseURL: 'http://www.pensivewright.com/serv'
+    baseURL: 'http://localhost:8081/serv'
   })
 }
