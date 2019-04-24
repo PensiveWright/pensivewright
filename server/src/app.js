2310a69c3857cd3504e495330d2ac15f21cdfa75
Kevin <tello3000@hotmail.com>
2019-04-23 22:17:14 -0400

    update

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,7 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors({origin: "*"}))
 
-/*
+
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/posts');
 var db = mongoose.connection;
@@ -18,38 +18,36 @@ db.on("error", console.error.bind(console, "connection error"));
 db.once("open", function(callback){
   console.log("Connection Succeeded");
 });
-*/
+
+
 app.get('/serv', (req, res) => {
   res.send(
     [{
       title: "Hello World!",
       description: "Hi there! How are you?"
     }]
-)
+  )
 })
 
 // Fetch all posts
 app.get('/serv/posts', (req, res) => {
-  
+  /*
   res.send({posts: {
     title: "First Post",
     description: "This is working, good job!"
   }})
-  
- /*
+  */
   Post.find({}, 'title description', function (error, posts) {
     if (error) { console.error(error); }
     res.send({
       posts: posts
     })
   }).sort({_id:-1})
-  */
 })
 
 
 // Add new post
 app.post('/serv/posts', (req, res) => {
-  /*
   var db = req.db;
   var title = req.body.title;
   var description = req.body.description;
@@ -66,7 +64,6 @@ app.post('/serv/posts', (req, res) => {
       message: 'Post saved successfully!'
     })
   })
-  */
 })
 
 
